0afee652c87d94cfaa497dabc8eb92b9a1edd2ec
Tesfatsion Shiferaw <62855279+Tesfa-eth@users.noreply.github.com>
2023-01-07 09:58:19 -0500

    complete products page

--- server/index.js
+++ server/index.js
@@ -12,7 +12,9 @@ import salesRoutes from "./routes/sales.js";
 
 // data imports
 import User from "./models/User.js";
-import { dataUser } from "./data/index.js";
+import Product from "./models/Product.js";
+import ProductStat from "./models/ProductStat.js";
+import { dataUser, dataProduct, dataProductStat } from "./data/index.js";
 
 /* CONFIGURATION */
 dotenv.config();
@@ -41,4 +43,6 @@ mongoose.connect(process.env.MONGO_URL, {
 
     /* ONLY ADD DATA ONE TIME */
     //User.insertMany(dataUser);
+    // Product.insertMany(dataProduct);
+    // ProductStat.insertMany(dataProductStat)
 }).catch((error) => console.log(`${error} did not connect`));
