1e894649a7bd49440a63c40350da87a021e3fb46
Tesfatsion Shiferaw <62855279+Tesfa-eth@users.noreply.github.com>
2023-01-07 12:05:59 -0500

    geography page done

--- server/controllers/client.js
+++ server/controllers/client.js
@@ -1,6 +1,7 @@
 import Product from "../models/Product.js";
 import ProductStat from "../models/ProductStat.js";
 import User from "../models/User.js";
+import getCountryIso3 from "country-iso-2-to-3";
 
 export const getProduct = async (req, res) => {
   try {
@@ -27,8 +28,32 @@ export const getProduct = async (req, res) => {
 
 export const getCustomers = async (req, res) => {
   try {
-    const customers = await User.find({ role: "user" }).select("-password");
+    const customers = await User.find().select("-password");
     res.status(200).json(customers);
+  } catch (error) {
+    res.status(404).json({ message: error.message });
+  }
+};
+
+export const getGeography = async (req, res) => {
+  try {
+    const user = await User.find();
+    const mappedLocations = user.reduce((acc, { country }) => {
+      const countryIso3 = getCountryIso3(country);
+      if (!acc[countryIso3]) {
+        acc[countryIso3] = 0;
+      }
+      acc[countryIso3]++;
+      return acc;
+    }, {});
+
+    const formattedLocations = Object.entries(mappedLocations).map(
+      ([country, count]) => {
+        return { id: country, value: count };
+      }
+    );
+
+    res.status(200).json(formattedLocations);
   } catch (error) {
     res.status(404).json({ message: error.message });
   }
