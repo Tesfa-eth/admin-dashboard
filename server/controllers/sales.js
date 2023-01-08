eacc25ca5e7b29f7ab887e0ecf391774a7a63833
Tesfatsion Shiferaw <62855279+Tesfa-eth@users.noreply.github.com>
2023-01-08 16:13:59 -0500

    completed overview page

---
+++ server/controllers/sales.js
@@ -0,0 +1,11 @@
+import OverallStat from "../models/overallStat.js";
+
+export const getSales = async (req, res) => {
+  try {
+    const overallStats = await OverallStat.find();
+
+    res.status(200).json(overallStats[0]);
+  } catch (error) {
+    res.status(400).json({ message: error.message });
+  }
+};
