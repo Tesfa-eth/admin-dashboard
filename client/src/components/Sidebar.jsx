59c305a42f1f97c5f36a312f8b2317ff3c3f87fb
Tesfatsion Shiferaw <62855279+Tesfa-eth@users.noreply.github.com>
2023-01-08 14:52:34 -0500

    complete transactions page

--- client/src/components/Sidebar.jsx
+++ client/src/components/Sidebar.jsx
@@ -1,196 +1,201 @@
-import React from 'react';
+import React from "react";
 import {
-    Box,
-    Divider,
-    Drawer,
-    IconButton,
-    List,
-    ListItem,
-    ListItemButton,
-    ListItemIcon,
-    ListItemText,
-    Typography,
-    useTheme
+  Box,
+  Divider,
+  Drawer,
+  IconButton,
+  List,
+  ListItem,
+  ListItemButton,
+  ListItemIcon,
+  ListItemText,
+  Typography,
+  useTheme,
 } from "@mui/material";
 
 import {
-    SettingsOutlined,
-    ChevronLeft,
-    ChevronRightOutlined,
-    HomeOutlined,
-    ShoppingCartOutlined,
-    Groups2Outlined,
-    ReceiptLongOutlined,
-    PublicOutlined,
-    PointOfSaleOutlined,
-    TodayOutlined,
-    CalendarMonthOutlined,
-    AdminPanelSettingsOutlined,
-    TrendingUpOutlined,
-    PieChartOutlined,
+  SettingsOutlined,
+  ChevronLeft,
+  ChevronRightOutlined,
+  HomeOutlined,
+  ShoppingCartOutlined,
+  Groups2Outlined,
+  ReceiptLongOutlined,
+  PublicOutlined,
+  PointOfSaleOutlined,
+  TodayOutlined,
+  CalendarMonthOutlined,
+  AdminPanelSettingsOutlined,
+  TrendingUpOutlined,
+  PieChartOutlined,
 } from "@mui/icons-material";
-import { useEffect, useState } from 'react';
-import { useLocation, useNavigate } from 'react-router-dom';
-import FlexBetween from './FlexBetween';
-import profileImage from "assets/profile.jpeg"
-import { width } from '@mui/system';
+import { useEffect, useState } from "react";
+import { useLocation, useNavigate } from "react-router-dom";
+import FlexBetween from "./FlexBetween";
+import profileImage from "assets/profile.jpeg";
+import { width } from "@mui/system";
 
 const navItems = [
-    {
-      text: "Dashboard",
-      icon: <HomeOutlined />,
-    },
-    {
-      text: "Client Facing",
-      icon: null,
-    },
-    {
-      text: "Products",
-      icon: <ShoppingCartOutlined />,
-    },
-    {
-      text: "Customers",
-      icon: <Groups2Outlined />,
-    },
-    {
-      text: "Transactions",
-      icon: <ReceiptLongOutlined />,
-    },
-    {
-      text: "Geography",
-      icon: <PublicOutlined />,
-    },
-    {
-      text: "Sales",
-      icon: null,
-    },
-    {
-      text: "Overview",
-      icon: <PointOfSaleOutlined />,
-    },
-    {
-      text: "Daily",
-      icon: <TodayOutlined />,
-    },
-    {
-      text: "Monthly",
-      icon: <CalendarMonthOutlined />,
-    },
-    {
-      text: "Breakdown",
-      icon: <PieChartOutlined />,
-    },
-    {
-      text: "Management",
-      icon: null,
-    },
-    {
-      text: "Admin",
-      icon: <AdminPanelSettingsOutlined />,
-    },
-    {
-      text: "Performance",
-      icon: <TrendingUpOutlined />,
-    },
-  ];
+  {
+    text: "Dashboard",
+    icon: <HomeOutlined />,
+  },
+  {
+    text: "Client Facing",
+    icon: null,
+  },
+  {
+    text: "Products",
+    icon: <ShoppingCartOutlined />,
+  },
+  {
+    text: "Customers",
+    icon: <Groups2Outlined />,
+  },
+  {
+    text: "Transactions",
+    icon: <ReceiptLongOutlined />,
+  },
+  {
+    text: "Geography",
+    icon: <PublicOutlined />,
+  },
+  {
+    text: "Sales",
+    icon: null,
+  },
+  {
+    text: "Overview",
+    icon: <PointOfSaleOutlined />,
+  },
+  {
+    text: "Daily",
+    icon: <TodayOutlined />,
+  },
+  {
+    text: "Monthly",
+    icon: <CalendarMonthOutlined />,
+  },
+  {
+    text: "Breakdown",
+    icon: <PieChartOutlined />,
+  },
+  {
+    text: "Management",
+    icon: null,
+  },
+  {
+    text: "Admin",
+    icon: <AdminPanelSettingsOutlined />,
+  },
+  {
+    text: "Performance",
+    icon: <TrendingUpOutlined />,
+  },
+];
 
 const Sidebar = ({
-    user,
-    drawerWidth,
-    isSidebarOpen,
-    setIsSidebarOpen,
-    isNonMobile
+  user,
+  drawerWidth,
+  isSidebarOpen,
+  setIsSidebarOpen,
+  isNonMobile,
 }) => {
-    const { pathname } = useLocation();
-    const [active, setActive] = useState("")
-    const navigate = useNavigate();
-    const theme = useTheme();
+  const { pathname } = useLocation();
+  const [active, setActive] = useState("");
+  const navigate = useNavigate();
+  const theme = useTheme();
 
-    useEffect(() => {
-        setActive(pathname.substring(1));
-    }, [pathname])
-  return <Box component="nav">
-    {isSidebarOpen && (
+  useEffect(() => {
+    setActive(pathname.substring(1));
+  }, [pathname]);
+  return (
+    <Box component="nav">
+      {isSidebarOpen && (
         <Drawer
-            open={isSidebarOpen}
-            onClose={() => setIsSidebarOpen(false)}
-            variant="persistent"
-            anchor='left'
-            sx={{
-                width: drawerWidth,
-                "& .MuiDrawer-paper":{
-                    color: theme.palette.secondary[200],
-                    background: theme.palette.background.alt,
-                    boxSizing: "border-box",
-                    borderWidth: isNonMobile ? 0 : "2px",
-                    width: drawerWidth
-                }
-            }}
+          open={isSidebarOpen}
+          onClose={() => setIsSidebarOpen(false)}
+          variant="persistent"
+          anchor="left"
+          sx={{
+            width: drawerWidth,
+            "& .MuiDrawer-paper": {
+              color: theme.palette.secondary[200],
+              background: theme.palette.background.alt,
+              boxSizing: "border-box",
+              borderWidth: isNonMobile ? 0 : "2px",
+              width: drawerWidth,
+            },
+          }}
         >
-            <Box width="100%">
-                <Box m="1.5rem 2rem 2rem 3rem">
-                    <FlexBetween color={theme.palette.secondary.main}>
-                        <Box display="flex" alignItems="center" gap="0.5rem">
-                            <Typography variant="h4" fontWeight="bold">
-                                ECOMVISION
-                            </Typography>
-                        </Box>
-                        {!isNonMobile && (
-                            <IconButton onClick={() => setIsSidebarOpen(!isSidebarOpen)}>
-                                <ChevronLeft />
-                            </IconButton>
-                        )}
-                    </FlexBetween>
+          <Box width="100%">
+            <Box m="1.5rem 2rem 2rem 3rem">
+              <FlexBetween color={theme.palette.secondary.main}>
+                <Box display="flex" alignItems="center" gap="0.5rem">
+                  <Typography variant="h4" fontWeight="bold">
+                    ECOMVISION
+                  </Typography>
                 </Box>
-                <List>
-                    { navItems.map(({ text, icon})  => {
-                        if(!icon){
-                            return (<Typography key={text} sx={{ m:"2.25rem 0 1rem 3rem"}}>
-                                {text}
-                            </Typography>
-                            )
-                        }
-
-                        const lcText  = text.toLowerCase();
-
-                        return (
-                            <ListItem key={text} disablePadding>
-                                <ListItemButton 
-                                onClick={() => 
-                                {navigate(`/${lcText}`);
-                                setActive(lcText);
-                            }}
-                            sx={{
-                                backgroundColor: active === lcText 
-                                    ? theme.palette.secondary[300] 
-                                    : "transparent",
-                                color: active === lcText 
-                                    ? theme.palette.primary[600] 
-                                    : theme.palette.secondary[100]
-                            }} >
-                                <ListItemIcon
-                                    sx={{
-                                        ml: "2rem",
-                                        color: active === lcText 
-                                        ? theme.palette.primary[600] 
-                                        : theme.palette.secondary[200]
-                                    }}
-                                >
-                                    {icon}
-                                </ListItemIcon>
-                                <ListItemText primary={text} />
-                                { active === lcText && (
-                                    <ChevronRightOutlined sx={{ ml: "auto" }} />
-                                )}
-                                </ListItemButton>
-                            </ListItem>
-                        )
-                        
-                    })}
-                </List>
+                {!isNonMobile && (
+                  <IconButton onClick={() => setIsSidebarOpen(!isSidebarOpen)}>
+                    <ChevronLeft />
+                  </IconButton>
+                )}
+              </FlexBetween>
             </Box>
+            <List>
+              {navItems.map(({ text, icon }) => {
+                if (!icon) {
+                  return (
+                    <Typography key={text} sx={{ m: "2.25rem 0 1rem 3rem" }}>
+                      {text}
+                    </Typography>
+                  );
+                }
 
-            <Box bottom="2rem">
+                const lcText = text.toLowerCase();
+
+                return (
+                  <ListItem key={text} disablePadding>
+                    <ListItemButton
+                      onClick={() => {
+                        navigate(`/${lcText}`);
+                        setActive(lcText);
+                      }}
+                      sx={{
+                        backgroundColor:
+                          active === lcText
+                            ? theme.palette.secondary[300]
+                            : "transparent",
+                        color:
+                          active === lcText
+                            ? theme.palette.primary[600]
+                            : theme.palette.secondary[100],
+                      }}
+                    >
+                      <ListItemIcon
+                        sx={{
+                          ml: "2rem",
+                          color:
+                            active === lcText
+                              ? theme.palette.primary[600]
+                              : theme.palette.secondary[200],
+                        }}
+                      >
+                        {icon}
+                      </ListItemIcon>
+                      <ListItemText primary={text} />
+                      {active === lcText && (
+                        <ChevronRightOutlined sx={{ ml: "auto" }} />
+                      )}
+                    </ListItemButton>
+                  </ListItem>
+                );
+              })}
+            </List>
+          </Box>
+
+          <Box bottom="2rem">
             <Divider />
             <FlexBetween textTransform="none" gap="1rem" m="1.5rem 2rem 0 3rem">
               <Box
@@ -226,8 +231,9 @@ const Sidebar = ({
             </FlexBetween>
           </Box>
         </Drawer>
-    )}
-  </Box>
-}
+      )}
+    </Box>
+  );
+};
 
-export default Sidebar
+export default Sidebar;
--- server/routes/client.js
+++ server/routes/client.js
@@ -3,6 +3,7 @@ import {
   getProduct,
   getCustomers,
   getGeography,
+  getTransaction,
 } from "../controllers/client.js";
 
 const router = express.Router();
@@ -10,5 +11,6 @@ const router = express.Router();
 router.get("/products", getProduct);
 router.get("/customers", getCustomers);
 router.get("/geography", getGeography);
+router.get("/transactions", getTransaction);
 
 export default router;
