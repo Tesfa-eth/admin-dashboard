92584a2823fb442041b6ac5906c76c68ff24ee1e
Tesfatsion Shiferaw <62855279+Tesfa-eth@users.noreply.github.com>
2023-01-11 13:19:02 -0500

    added performance api

--- server/index.js
+++ server/index.js
@@ -16,6 +16,7 @@ import Product from "./models/Product.js";
 import ProductStat from "./models/ProductStat.js";
 import Transaction from "./models/Transaction.js";
 import OverallStat from "./models/overallStat.js";
+import AffiliateStat from "./models/AffiliateStat.js";
 
 import {
   dataUser,
@@ -23,6 +24,7 @@ import {
   dataProductStat,
   dataTransaction,
   dataOverallStat,
+  dataAffiliateStat,
 } from "./data/index.js";
 
 /* CONFIGURATION */
@@ -58,5 +60,6 @@ mongoose
     // ProductStat.insertMany(dataProductStat)
     //Transaction.insertMany(dataTransaction);
     //OverallStat.insertMany(dataOverallStat);
+    //AffiliateStat.insertMany(dataAffiliateStat);
   })
   .catch((error) => console.log(`${error} did not connect`));
