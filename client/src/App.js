4f19fae8bbb0defbd46a7761fb299599360710ca
Tesfatsion Shiferaw <62855279+Tesfa-eth@users.noreply.github.com>
2023-01-11 14:16:50 -0500

    update App.js

--- client/src/App.js
+++ client/src/App.js
@@ -15,6 +15,7 @@ import Daily from "scenes/daily";
 import Monthly from "scenes/monthly";
 import Breakdown from "scenes/breakdown";
 import Admins from "scenes/admins";
+import Performance from "scenes/performance";
 
 function App() {
   const mode = useSelector((state) => state.global.mode);
@@ -38,6 +39,7 @@ function App() {
               <Route path="/monthly" element={<Monthly />} />
               <Route path="/breakdown" element={<Breakdown />} />
               <Route path="/admin" element={<Admins />} />
+              <Route path="/performance" element={<Performance />} />
             </Route>
           </Routes>
         </ThemeProvider>
